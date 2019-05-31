ccf466f7c2630e8b1278b43396630fbddb42ea59
David Sabalete <dsabalete@gmail.com>
2019-05-31 13:27:17 +0200

    Refs with React Hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import classes from './Cockpit.module.scss'
 
-const Cockpit = (props) => {
+const Cockpit = props => {
+    const toggleBtnRef = useRef(null)
+
     useEffect(() => {
         console.log('[Cockpit.js] useEffect')
+        // Http request...
+        // setTimeout(() => {
+        //     alert('Saved data to cloud!')
+        // }, 1000)
+        toggleBtnRef.current.click()
+        return () => {
+            console.log('[Cockpit.js] cleanup work in useEffect')
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log('[Cockpit.js] 2nd useEffect')
+        return () => {
+            console.log('[Cockpit.js] cleanup work in 2nd useEffect')
+        }
     });
 
     const assignedClasses = [];
@@ -25,10 +42,13 @@ const Cockpit = (props) => {
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
+                ref={toggleBtnRef}
                 className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
+                onClick={props.clicked}>
+                Toggle Persons
+            </button>
         </div>
     )
 }
 
-export default Cockpit
+export default React.memo(Cockpit)
